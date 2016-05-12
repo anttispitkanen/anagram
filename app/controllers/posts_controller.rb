e808b6b3e1c40425e11ca35c6b3750e36bbb586d
Antti <anttis.pitkanen@gmail.com>
2016-05-12 22:26:11 +0300

    chapter 1 completed, page 42

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
@@ -10,8 +13,32 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Post couldn't be created. Please check the form."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Your post was updated!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post deleted."
+    redirect_to root_path
   end
 
 
@@ -19,6 +46,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 
