a83bf7ce643f177714431c5b65a1e974163a8995
Antti <anttis.pitkanen@gmail.com>
2016-05-13 17:40:57 +0300

    page 60

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ActiveRecord::Base
-
+  validates :user_id, presence: true
   validates :image, presence: true
   validates :caption, presence: true, length: { minimum: 1, maximum: 1600 }
-                                                #or should it be "640px" ?
-  has_attached_file :image, styles: { :medium => "640x" }
+
+  belongs_to :user
+
+
+  has_attached_file :image, styles: { :medium => "640x" } #or should it be "640px" ?
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
